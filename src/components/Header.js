04835f5f4fcdce8453b087d3c0e3d314c2b6956f
Victor Peinado <vpeinado263@gmail.com>
2024-10-26 16:11:35 -0300

    Finalizar botonde Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import { ThemeContext } from "@/context/ThemeContextProvider";
+import { useContext } from "react";
+import Image from 'next/image'; // Importa el componente Image de next/image
 
 const Header = () => {
+  const data = useContext(ThemeContext);
+  const [toggleTheme, handleTheme] = data;
+
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header className={toggleTheme}>
+      <button onClick={handleTheme}>
+        <Image
+          src={toggleTheme === "light" ? "/sunDark.png" : "/sun.png"} //Ternario
+          alt="theme-logos"
+          width={30}
+          height={30}
+        />
+      </button>
+      <style jsx>{`
+        button {
+          background-color: transparent;
+          border: none;
+        }
+        img {
+        width: 30px;
+        }
+      `}</style>
+    </header>
+  );
+};
 
 export default Header;
