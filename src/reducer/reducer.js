853e9ac5fb9d9965a10d207a99374def33285fc2
Victor Peinado <vpeinado263@gmail.com>
2024-10-26 17:11:29 -0300

    Implementar archivo reducer para utilizarlo de manera global

---
+++ src/reducer/reducer.js
@@ -0,0 +1,26 @@
+import { initialState } from '@/reducer/initialState';
+import { TYPES } from '@/actions/actions';
+
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case TYPES.INCREMENTAR: {
+            return {
+                ...state,
+                contador: state.contador + action.payload
+               }
+        }
+        case TYPES.RESETEAR: {
+            return initialState
+        }
+        case TYPES.DECREMENTAR: {
+            return {
+                ...state,
+                contador: state.contador - action.payload
+               }
+        }
+
+        default:
+            return state;
+    }
+}
