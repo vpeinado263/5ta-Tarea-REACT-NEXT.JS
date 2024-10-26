87edc7dc887e5801355d763a86a531c70ebe3dd0
Victor Peinado <vpeinado263@gmail.com>
2024-10-26 15:08:49 -0300

    Configurar el contexto de theme

--- src/context/ThemeContextProvider.js
+++ src/context/ThemeContextProvider.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import { createContext, useState } from "react"
 
-const ThemeContextProvider = () => {
-  return (
-    <div>
-      
-    </div>
+export const ThemeContext = createContext()
+
+const ThemeContextProvider = ({children}) => {
+
+    const [theme, setTheme] = useState(false)
+//para que funcione de la otra forma colocamos una e entre parentesis()
+
+    const handleTheme = () => {
+       
+        /*queremos acctualizar el estado dependiendo de que input del formulario estemos clickenado*/
+
+        /*sugerencia de cambiar ek  estado en toggle teme*/
+
+        // if ( e.target.value === "light") {
+        //     setTheme("ligth")
+        // } else {
+        //     setTheme("dark")
+        // }
+
+
+        setTheme(!theme)
+    }
+
+    const toggleTheme = theme ? "dark" : "light"
+
+    const data = [toggleTheme, handleTheme]
+  
+    return (
+    <ThemeContext.Provider value={data}>
+      {children}
+    </ThemeContext.Provider>
   )
 }
 
 export default ThemeContextProvider
+
