cfd0aa8b8e10c87baed5df04f51eee765307d5fd
Victor Peinado <vpeinado263@gmail.com>
2024-10-26 14:47:44 -0300

    Referenciar componentes a index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import Head from "next/head";
 
 export default function Home() {
@@ -9,7 +11,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
- 
+      <Header/>
+      <main>
+        <Home/>
+      </main>
+      <Footer/>
     </>
   );
 }
