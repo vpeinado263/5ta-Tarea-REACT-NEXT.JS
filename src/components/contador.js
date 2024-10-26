df50519402145155b6c6af9a98e249a8094bd2b0
Victor Peinado <vpeinado263@gmail.com>
2024-10-26 17:16:54 -0300

    Finalizar contador y separacion de acciones para utilizar de manera global

--- src/components/contador.js
+++ src/components/contador.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import {useReducer} from 'react'
+import { TYPES } from '@/actions/actions';
+import { initialState } from '@/reducer/initialState';
+import { reducer } from '@/reducer/reducer';
 
-const contador = () => {
+
+const Contador = () => {
+
+   const [state, dispatch] = useReducer(reducer, initialState)
+
+   const sumar = () => dispatch({type: TYPES.INCREMENTAR, payload: 1000})
+
+   const resetear = () => dispatch({type: TYPES.RESETEAR})
+
+   const restar = () => dispatch({type: TYPES.DECREMENTAR, payload: 1000})
+   
+  
+
   return (
-    <div>
-      
+    <div className="counter-container">
+        <div className="counter-menu">
+            <button onClick={sumar}>+</button>
+            <button onClick={resetear}>0</button>
+            <button onClick={restar}>-</button>
+        </div>
+        <h3>{state.contador}</h3>
+
+        <style jsx>{`
+        .counter-container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        .counter-menu {
+            display: flex;
+        }
+        `}</style>
     </div>
   )
 }
 
-export default contador
+export default Contador
